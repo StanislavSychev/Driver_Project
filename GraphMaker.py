5f1eb60ae344b04da36a409d2383209e6c5e814b
stan <sychevstanislav@gmail.com>
2018-12-06 17:33:24 +0300

    add svm graph

--- GraphMaker.py
+++ GraphMaker.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from DesTree import run_des_tree
+from Predictor import run_predict
+from ScenariosRader import make_files
 
 
-def make_graph(x_list, y_list, err_list, pred_name):
+def make_graph(x_list, y_list, e_list, pred_name):
     x = np.array(x_list)
     y = np.array(y_list)
-    err = np.array(err_list)
+    e = np.array(e_list)
 
     fig, ax = plt.subplots()
-    ax.errorbar(x, y, yerr=err, fmt="ok", capsize=5)
+    ax.errorbar(x, y, yerr=e, fmt="ok", capsize=5)
     ax.plot(x, y, 'k')
 
     ax.set(xlabel='number of questionnaire features', ylabel='accuracy',
@@ -21,9 +23,15 @@ def make_graph(x_list, y_list, err_list, pred_name):
 
 
 if __name__ == '__main__':
+    tree = False
     make_files("procData2", 20, 20, 10, 20)
     _, _, quest_list = run_des_tree(False, True)
-    max_res, err, _ = run_des_tree(False, False)
+    if tree:
+        max_res, err, _ = run_des_tree(False, False)
+        name = 'ID3'
+    else:
+        max_res, err = run_predict(False, False)
+        name = 'SVM'
     max_num = 0
     res_list = [max_res]
     err_list = [err]
@@ -32,9 +40,13 @@ if __name__ == '__main__':
     needed = []
     while j < len(quest_list):
         needed.append(quest_list[j][0])
+        print j
         if quest_list[j][1] == 0:
             break
-        res, err, _ = run_des_tree(False, True, needed)
+        if tree:
+            res, err, _ = run_des_tree(False, True, needed)
+        else:
+            res, err = run_predict(False, True, needed)
         j += 1
         if res > max_res:
             max_res = res
@@ -43,4 +55,4 @@ if __name__ == '__main__':
         res_list.append(res)
         err_list.append(err)
 
-    make_graph(num_list, res_list, err_list, 'ID3')
+    make_graph(num_list, res_list, err_list, name)
--- Predictor.py
+++ Predictor.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.impute import SimpleImputer
 from DataPreparator import make_part_list
 from ScenariosRader import make_files
@@ -14,72 +13,83 @@ def get_score(predictor, x_train, y_train, x_test, y_test):
     predictor.fit(x_train, y_train)
     ac_score = 0
     for x, y in zip(x_test, y_test):
-        ac_score += pred.score([x], [y])
+        ac_score += predictor.score([x], [y])
     return ac_score / len(x_test)
 
 
-make_files("procData2", 20, 20, 10, 20)
-data = make_part_list(True)
-scens = data['scen']
-data = data['data']
-feat_imp = {}
-acc_res = {}
-for i in range(len(scens)):
-    accuracy = 0
-    ac_list = []
-    X_dict = {}
-    Y_dict = {}
-    names = []
-    for keys in data:
-        # XY = data[keys].scens_to_list(i)
-        XY = data[keys].most_prob(i)
-        if XY:
-            X_dict[keys] = XY[0]
-            Y_dict[keys] = XY[1]
-            names = XY[2]
-    driver_keys = X_dict.keys()
-    kf = KFold(n_splits=10, shuffle=True, random_state=42)
-    kf.get_n_splits(driver_keys)
-    dtr = DecisionTreeClassifier()
-    for train, test in kf.split(driver_keys):
-        test_keys = [driver_keys[i_test] for i_test in test.tolist()]
-        train_keys = [driver_keys[i_train] for i_train in train.tolist()]
-        X_train = []
-        Y_train = []
-        X_test = []
-        Y_test = []
-        for keys in X_dict:
-            if keys in train_keys:
-                for lst in X_dict[keys]:
-                    X_train.append(lst)
-                for lst in Y_dict[keys]:
-                    Y_train.append(lst)
-            else:
-                for lst in X_dict[keys]:
-                    X_test.append(lst)
-                for lst in Y_dict[keys]:
-                    Y_test.append(lst)
+def run_predict(to_print, to_quest=False, needed_quest=None):
+    data = make_part_list(to_quest, needed_quest)
+    scens = data['scen']
+    data = data['data']
+    # feat_imp = {}
+    acc_res = {}
+    av_ac = 0
+    av_ci = 0
+    for i in range(len(scens)):
+        accuracy = 0
+        ac_list = []
+        feat_dict = {}
+        res_dict = {}
+        # names = []
+        for keys in data:
+            # feat_res = data[keys].scens_to_list(i)
+            feat_res = data[keys].most_prob(i)
+            if feat_res:
+                feat_dict[keys] = feat_res[0]
+                res_dict[keys] = feat_res[1]
+                # names = feat_res[2]
+        driver_keys = feat_dict.keys()
+        kf = KFold(n_splits=10, shuffle=True, random_state=42)
+        kf.get_n_splits(driver_keys)
+        for train, test in kf.split(driver_keys):
+            # test_keys = [driver_keys[i_test] for i_test in test.tolist()]
+            train_keys = [driver_keys[i_train] for i_train in train.tolist()]
+            feat_train = []
+            res_train = []
+            feat_test = []
+            res_test = []
+            for keys in feat_dict:
+                if keys in train_keys:
+                    for lst in feat_dict[keys]:
+                        feat_train.append(lst)
+                    for lst in res_dict[keys]:
+                        res_train.append(lst)
+                else:
+                    for lst in feat_dict[keys]:
+                        feat_test.append(lst)
+                    for lst in res_dict[keys]:
+                        res_test.append(lst)
 
-        # print scens[i]
-        # print len(X_test[0])
-        imp = SimpleImputer(missing_values=-1, strategy='mean')
-        X_train.append([0]*len(X_train[0]))
-        imp.fit(X_train)
-        X_train = imp.transform(X_train)
-        X_train = X_train[:-1:]
-        # imp.fit(X_test)
-        X_test = imp.transform(X_test)
-        pred = SVC()
-        # pred = GaussianNB()
-        ac = get_score(pred, X_train, Y_train, X_test, Y_test)
-        ac_list.append(ac)
-        accuracy += ac
-    accuracy = accuracy / 10
-    err = 0.0
-    for item in ac_list:
-        err += (item - accuracy) ** 2
-    err = (err ** 0.5) / 10
-    acc_res[scens[i]] = scens[i] + ': ' + '%.2f' % accuracy + '\t%.2f' % err
+            # print scens[i]
+            # print len(feat_test[0])
+            imp = SimpleImputer(missing_values=-1, strategy='mean')
+            feat_train.append([0] * len(feat_train[0]))
+            imp.fit(feat_train)
+            feat_train = imp.transform(feat_train)
+            feat_train = feat_train[:-1:]
+            # imp.fit(feat_test)
+            feat_test = imp.transform(feat_test)
+            pred = SVC(gamma='scale')
+            # pred = GaussianNB()
+            ac = get_score(pred, feat_train, res_train, feat_test, res_test)
+            ac_list.append(ac)
+            accuracy += ac
+        accuracy = accuracy / 10
+        err = 0.0
+        for item in ac_list:
+            err += (item - accuracy) ** 2
+        err = (err ** 0.5) / 10
+        acc_res[scens[i]] = scens[i] + ': ' + '%.2f' % accuracy + '\t%.2f' % err
+        av_ac += accuracy
+        av_ci += err
+    if to_print:
+        for keys in acc_res:
+            print acc_res[keys]
+    av_ci = av_ci / 12
+    av_ac = av_ac / 12
+    return av_ac, av_ci
 
-for keys in acc_res:
-    print acc_res[keys]
+
+if __name__ == '__main__':
+    make_files("procData2", 20, 20, 10, 20)
+    run_predict(True, True)
